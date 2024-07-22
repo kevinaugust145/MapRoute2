f1ee8b21476dd7fb0f5797e96ba8513ff4de881d
KevinHung <kevinaugust145@gmail.com>
2024-07-22 13:43:48 +0800

    Initial Commit

---
+++ MapRoute2/MapRoute2App.swift
@@ -0,0 +1,17 @@
+//
+//  MapRoute2App.swift
+//  MapRoute2
+//
+//  Created by 洪宗燦 on 2024/7/22.
+//
+
+import SwiftUI
+
+@main
+struct MapRoute2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
